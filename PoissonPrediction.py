77dcbb5eb03de9d5df293b70d818702b6f3068e7
mackaveli07 <60677021+mackaveli07@users.noreply.github.com>
2020-03-01 18:03:07 -0600

    Update PoissonPrediction.py
    updated the program to do 2 teams at a time

--- PoissonPrediction.py
+++ PoissonPrediction.py
@@ -1,9 +1,17 @@
 from scipy.stats import poisson
 import seaborn as sb
 
-expectedTD = input("Enter the expected amount of TDs that the team will score")
+HomeexpectedTD = input("Enter the expected amount of TDs that the home team will score")
+AwayxpectedTD = input("Enter the expected amount of TDs that the away team will score")
 
-data_binom = poisson.rvs(mu=expectedTD, size=10000)
+data_binom = poisson.rvs(mu=HomeexpectedTD, size=10000)
+ax = sb.distplot(data_binom,
+                  kde=True,
+                  color='green',
+                  hist_kws={"linewidth": 25,'alpha':1})
+ax.set(xlabel='TDs', ylabel='Probability')
+
+data_binom = poisson.rvs(mu=AwayexpectedTD, size=10000)
 ax = sb.distplot(data_binom,
                   kde=True,
                   color='green',
