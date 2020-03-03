5806c204a233fe83967fff3932d32b87e92ee843
mackaveli07 <60677021+mackaveli07@users.noreply.github.com>
2020-03-03 14:11:23 -0600

    Update PoissonPrediction.py
    minor error fixes

--- PoissonPrediction.py
+++ PoissonPrediction.py
@@ -5,23 +5,22 @@ import seaborn as sb
 HomeExpectedTD = input("Enter the expected amount of TDs that the home team will score")
 AwayExpectedTD = input("Enter the expected amount of TDs that the away team will score")
 
-data_binom = poisson.rvs(mu=HomeexpectedTD, size=10000)
+data_binom = poisson.rvs(mu=HomeExpectedTD, size=10000)
 ax = sb.distplot(data_binom,
                   kde=True,
                   color='green',
                   hist_kws={"linewidth": 25,'alpha':1})
 ax.set(xlabel='TDs', ylabel='Probability')
 
-data_binom = poisson.rvs(mu=AwayexpectedTD, size=10000)
-=======
-data_binom = poisson.rvs(mu=4, size=10000)
->>>>>>> cb2a2b6d4d59c6e2cac313d8f5bce96d2bc97feb
+
+
+data_binom2 = poisson.rvs(mu=AwayExpectedTD, size=10000)
+
 ax = sb.distplot(data_binom,
                   kde=True,
                   color='green',
                   hist_kws={"linewidth": 25,'alpha':1})
-<<<<<<< HEAD
+
 ax.set(xlabel='TDs', ylabel='Probability')
-=======
-ax.set(xlabel='Poisson', ylabel='Frequency')
->>>>>>> cb2a2b6d4d59c6e2cac313d8f5bce96d2bc97feb
+
+
